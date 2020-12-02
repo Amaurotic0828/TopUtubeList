ce30300682c172069c2216d25dfca8e2cc392533
Amaurotic <shlmaesntscbrl@gmail.com>
2020-12-02 13:30:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/toplist/Modify_List.kt
@@ -0,0 +1,11 @@
+package com.example.toplist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Modify_List : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modify__list)
+    }
+}
